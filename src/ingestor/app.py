dfb5750290b6244d7e9ec3f9a96475732c35327e
Jerome Swannack <jeromeswannack@gmail.com>
2023-04-22 14:22:34 +1200

    Added index to ingestor test service

--- src/ingestor/app.py
+++ src/ingestor/app.py
@@ -6,7 +6,33 @@ app = Flask(__name__)
 
 _recieved_data = defaultdict(list)
 
-@app.route('/', defaults={'path': ''}, methods=['POST'])
+# # add index / root route, that returns html with clickable links to all paths in _recieved_data
+# @app.route('/', defaults={'path': ''}, methods=['GET'])
+# def handle_get_request(path):
+
+
+@app.route('/', defaults={'path': ''}, methods=['GET'])
+@app.route('/<path:path>', methods=['GET'])
+def handle_get_request(path):
+    print(f"Sender IP: {request.remote_addr}")
+    print(f"Path: {path}")
+
+    if path == "":
+        html = "<html><body><h1>Available paths:</h1><ul>"
+
+        for path in _recieved_data:
+            # with the number of data entries
+            html += f"<li><a href=\"{path}\">{path}</a> ({len(_recieved_data[path])} entries)</li>"
+
+        html += "</ul></body></html>"
+
+        return html, 200
+
+    if path in _recieved_data:
+        return _recieved_data[path], 200
+    else:
+        return "No data found", 404
+
 @app.route('/<path:path>', methods=['POST'])
 def handle_post_request(path):
     request_data = request.get_data()
@@ -22,15 +48,6 @@ def handle_post_request(path):
     return "Request received", 200
 
 
-@app.route('/<path:path>', methods=['GET'])
-def handle_get_request(path):
-    print(f"Sender IP: {request.remote_addr}")
-    print(f"Path: {path}")
-
-    if path in _recieved_data:
-        return _recieved_data[path], 200
-    else:
-        return "No data found", 404
 
 
 @app.route('/<path:path>', methods=['DELETE'])
