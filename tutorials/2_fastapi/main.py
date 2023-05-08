1b6722d931ba90800414c0ec76f2b4878a628dbe
Samueljbk <samjbkyle@gmail.com>
2023-05-08 18:24:51 +1200

    FastAPI tutorials

--- tutorials/2_fastapi/main.py
+++ tutorials/2_fastapi/main.py
@@ -1,2 +1,25 @@
 # Scratch pad when working on this tutorial:
 # https://fastapi.tiangolo.com/tutorial/
+
+from enum import Enum
+from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
