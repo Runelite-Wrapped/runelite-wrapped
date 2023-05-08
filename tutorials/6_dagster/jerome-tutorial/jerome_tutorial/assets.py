4c53abde41b70feb549a1e2dae26055db5c1d4ad
Jerome Swannack <jeromeswannack@gmail.com>
2023-05-08 14:55:57 +1200

    Added my dagster tutorial first asset

---
+++ tutorials/6_dagster/jerome-tutorial/jerome_tutorial/assets.py
@@ -0,0 +1,10 @@
+import requests
+from dagster import asset
+
+
+@asset
+def topstory_ids():
+    newstories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+    top_new_story_ids = requests.get(newstories_url).json()[:100]
+    return top_new_story_ids
+
