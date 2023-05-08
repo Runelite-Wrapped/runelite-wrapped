f8a26d87650152071330d7676cc850295ca1252f
Samueljbk <samjbkyle@gmail.com>
2023-05-08 12:06:24 +1200

    Added mongo db

--- tutorials/3_mongo_deployment/main.sh
+++ tutorials/3_mongo_deployment/main.sh
@@ -1,2 +1,5 @@
 # Scratch pad when working on this tutorial:
 # https://www.linode.com/docs/guides/set-up-mongodb-on-docker/
+
+# sudo docker run --name mongo_example -d mongo:6.0.5
+# sudo docker run --name rl-mongo -e MONGO_INITDB_ROOT_USERNAME=$MONGO_USER -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_PASSWORD -p 27017:27017 -d mongo:6.0.5
