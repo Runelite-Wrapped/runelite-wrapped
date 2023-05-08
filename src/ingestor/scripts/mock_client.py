43c1d531c07703968cd97cd8b7d2ea89cff78c91
Jerome Swannack <jeromeswannack@gmail.com>
2023-05-08 14:29:53 +1200

    Turned api seed script into a mock client

--- src/ingestor/scripts/mock_client.py
+++ src/ingestor/scripts/mock_client.py
@@ -1,7 +1,4 @@
-from typing import List
-from pathlib import Path
 import time
-import json
 import requests
 
 _URL = "http://localhost:8000/api/v1/event/game-tick/"
@@ -37,27 +34,6 @@ def _make_sample_data(index: int) -> dict:
         "timestamp": 1683158133517 + index*600,
         "username": "jerome-o"
     }
-
-
-
-
-_script_dir = Path(__file__).parent.absolute()
-_data_dir = _script_dir.parent / 'data'
-
-
-def _load_data(filename) -> List[dict]:
-    """Load file using json"""
-    with open(filename) as f:
-        return json.load(f)
-
-
-def main():
-    data = _load_data(_data_dir / "test_game_ticks.json")
-
-    for tick_data in data:
-        response = requests.post(_URL, json=tick_data)
-        print(response.status_code)
-        print(response.text)
 
 
 def seed_forever():
