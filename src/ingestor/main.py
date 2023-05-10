01e12a270c26fb53ca0ac217171a34fcb621c1d6
Samueljbk <samjbkyle@gmail.com>
2023-05-10 21:32:31 +1200

    first attempt on docker file

--- src/ingestor/main.py
+++ src/ingestor/main.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from pymongo import MongoClient
+from pydantic import BaseModel
 
 load_dotenv()
 
@@ -15,7 +16,23 @@ game_tick_collection = db.get_collection("game_ticks")
 app = FastAPI()
 
 
+class GameTickData(BaseModel):
+    energy: int
+    health: int
+    prayer: int
+    sessionTickCount: int
+    x: int
+    y: int
+
+
+class GameEventData(BaseModel):
+    data: GameTickData
+    event: str
+    timestamp: int
+    username: str
+
+
 @app.post("/api/v1/event/game-tick/")
-async def game_tick(event: dict):
-    print(game_tick_collection.insert_one(event))
+async def game_tick(event: GameEventData):
+    print(game_tick_collection.insert_one(event.dict()))
     return {"message": "OK"}
