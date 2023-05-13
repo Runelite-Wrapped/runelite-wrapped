000ec2fcec223da82ce307196c8b73c5d187e6d9
Jerome Swannack <jeromeswannack@gmail.com>
2023-05-14 11:24:14 +1200

    Wired up HomeView better

--- src/webapp/fe/src/views/HomeView.vue
+++ src/webapp/fe/src/views/HomeView.vue
@@ -1,21 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    <StatDisplay />
+    <img
+      alt="Vue logo"
+      src="../assets/runelite_wrapped_icon_wide.png"
+      class="top-image"
+    />
+    <div class="main-container">
+      <StatDisplay />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 import StatDisplay from "@/components/StatDisplay.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    HelloWorld,
     StatDisplay,
   },
 });
 </script>
+
+<style lang="css" scoped>
+.top-image {
+  width: 20%;
+  top: 0;
+  left: 0;
+}
+
+.main-container {
+  display: grid;
+  place-items: center;
+}
+</style>
