0db01e6adc5c796bed0090d4527985981a55d81c
Samueljbk <samjbkyle@gmail.com>
2023-05-15 18:15:19 +1200

    Added tick collection to analytics database

--- src/analytics/analytics/timeplayed.py
+++ src/analytics/analytics/timeplayed.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
@@ -10,20 +11,32 @@ load_dotenv()
 URI = os.environ["MONGO_URI"]
 
 client = MongoClient(URI)
-db = client.get_database("runelite-wrapped-raw")
-game_tick_collection = db.get_collection("game_ticks")
-total_tick_collection = db.get_collection("analytics.total_ticks")
+db_raw = client.get_database("runelite-wrapped-raw")
+db_analytics = client.get_database("runelite-wrapped-analytics")
+game_tick_collection = db_raw.get_collection("game_ticks")
+total_tick_collection = db_analytics.get_collection("tick_count")
 
 
 def pull_game_tick_data(username: str):
-    # Pull all game tick data for a certain player (username)
-    # Calculate / count the number of gameticks
-    # Push that to the analytics collection (total_tick_collection)
+    # Pull all game tick data for a certain player (username) and Calculate / count the number of gameticks
+    game_tick_count = game_tick_collection.count_documents({"username": username})
 
-    # {
-    # "stat": "total_ticks",
-    # "timestamp": "{time stamp of calculation} i.e. use `import time; time.time()`",
-    # "value": 23423,
-    # }
+    # Push that to the analytics collection (analytics.total_ticks)
+    output = {
+        "timestamp": time.time(),
+        "value": game_tick_count,
+        "username": username,
+    }
 
-    pass
+    total_tick_collection.insert_one(output)
+
+
+def main():
+    pull_game_tick_data("jerome-o")
+
+
+if __name__ == "__main__":
+    import logging
+
+    logging.basicConfig(level=logging.INFO)
+    main()
