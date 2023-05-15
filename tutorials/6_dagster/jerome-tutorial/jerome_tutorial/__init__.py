e7790a3b8fc424aa6328945ec901bf3ae6921d79
Jerome Swannack <jeromeswannack@gmail.com>
2023-05-15 13:30:21 +1200

    Completed up to schedules in dagster tutorial -jerome

--- tutorials/6_dagster/jerome-tutorial/jerome_tutorial/__init__.py
+++ tutorials/6_dagster/jerome-tutorial/jerome_tutorial/__init__.py
@@ -1,9 +1,26 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import (
+    AssetSelection,
+    Definitions,
+    ScheduleDefinition,
+    DefaultScheduleStatus,
+    define_asset_job,
+    load_assets_from_modules,
+)
 
 from . import assets
 
 all_assets = load_assets_from_modules([assets])
 
+hackernews_job = define_asset_job("hackernew_job", selection=AssetSelection.all())
+
+hackernews_shcedule = ScheduleDefinition(
+    job=hackernews_job,
+    cron_schedule="0 * * * *",
+    default_status=DefaultScheduleStatus.RUNNING,
+)
+
 defs = Definitions(
     assets=all_assets,
+    jobs=[hackernews_job],
+    schedules=[hackernews_shcedule],
 )
