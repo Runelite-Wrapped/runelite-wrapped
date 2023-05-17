928202d9076c65fc9f4b3776cdd687fcfcfcc6c9
Jerome Swannack <jeromeswannack@gmail.com>
2023-05-18 08:41:25 +1200

    Added proxy in vue config for /api passing to port 8000

--- src/webapp/fe/vue.config.js
+++ src/webapp/fe/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8000",
+        timeout: 6000,
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
 });
