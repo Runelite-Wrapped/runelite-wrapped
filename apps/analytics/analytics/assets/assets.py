a8c35b7b64b62ea909195deffdcede80d32e6808
Jerome Swannack <jeromeswannack@gmail.com>
2023-09-21 21:08:23 +1200

    Added some logging to the tick_count asset

--- apps/analytics/analytics/assets/assets.py
+++ apps/analytics/analytics/assets/assets.py
@@ -5,10 +5,12 @@ from analytics.processing.tick_count import (
     load_all_user_tick_counts,
 )
 
-logger = get_dagster_logger(__name__)
+_logger = get_dagster_logger(__name__)
 
 
 @asset
 def tick_count():
     tick_counts = calculate_all_user_tick_counts()
+    _logger.info(f"Calculated tick counts for {len(tick_counts)} users")
     load_all_user_tick_counts(tick_counts)
+    _logger.info(f"Loaded tick counts for {len(tick_counts)} users")
