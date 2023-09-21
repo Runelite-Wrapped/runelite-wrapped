70b16dcb3909c460ed88fea0baa8c13e289be459
Jerome Swannack <jeromeswannack@gmail.com>
2023-09-21 21:07:47 +1200

    Added __init__ files, removed hey asset

--- apps/analytics/analytics/__init__.py
+++ apps/analytics/analytics/__init__.py
@@ -1,6 +1,6 @@
 from dagster import Definitions, load_assets_from_modules
 
-from . import assets
+from analytics.assets import assets
 
 all_assets = load_assets_from_modules([assets])
 
--- apps/analytics/analytics/assets/assets.py
+++ apps/analytics/analytics/assets/assets.py
@@ -1,6 +1,6 @@
 from dagster import asset, get_dagster_logger
 
-from analytics.tick_count import (
+from analytics.processing.tick_count import (
     calculate_all_user_tick_counts,
     load_all_user_tick_counts,
 )
@@ -12,11 +12,3 @@ logger = get_dagster_logger(__name__)
 def tick_count():
     tick_counts = calculate_all_user_tick_counts()
     load_all_user_tick_counts(tick_counts)
-
-
-@asset
-def hey(tick_count: dict):
-    logger.info("hey this is the pipeline running")
-    logger.info("goodbye")
-    logger.info(tick_count)
-    print("hey")
