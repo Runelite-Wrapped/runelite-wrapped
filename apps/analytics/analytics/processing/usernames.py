0ca2ba384d408a20f549ac141b23841eb1bdb661
Jerome Swannack <jeromeswannack@gmail.com>
2023-09-26 08:35:09 +1300

    Added usernames asset

---
+++ apps/analytics/analytics/processing/usernames.py
@@ -0,0 +1,6 @@
+from typing import List
+from analytics.mongo import RawDbClient
+
+
+def get_all_usernames(raw_db_client: RawDbClient) -> List[str]:
+    return list(raw_db_client.get_game_tick_collection().distinct("username"))
