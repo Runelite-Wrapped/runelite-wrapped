8b565943b22bfd55eea22866c72bbf59f2476fc5
Jerome Swannack <jeromeswannack@gmail.com>
2023-09-25 19:39:14 +1300

    Added mongo clients as resources

--- apps/analytics/analytics/__init__.py
+++ apps/analytics/analytics/__init__.py
@@ -1,9 +1,11 @@
 from dagster import Definitions, load_assets_from_modules
 
 from analytics.assets import assets
+from analytics.resources import RESOURCES
 
 all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
     assets=all_assets,
+    resources=RESOURCES,
 )
