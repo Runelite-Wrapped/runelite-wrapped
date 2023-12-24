efbd56068157b2a90cddb720060f2b9b32666609
Jerome Swannack <jeromeswannack@gmail.com>
2023-12-24 17:37:08 +1300

    Using latest db for testing, figured out py->js api

--- prototypes/stats/frontend/site/python/read_db.py
+++ prototypes/stats/frontend/site/python/read_db.py
@@ -1,11 +1,24 @@
 # loads in sqlite db and lists the tables
 
+import json
 import sqlite3
 
 conn = sqlite3.connect("/stat.db")
 
 c = conn.cursor()
 
-c.execute("SELECT * FROM users")
+c.execute("SELECT JSON(data) -> '$.timestamp' FROM game_tick")
 print("hello")
-print(c.fetchall())
+result = c.fetchall()
+
+timestamps = [int(x[0]) for x in result]
+print(timestamps)
+
+
+# TODO: find a way to pass data between without having
+#   to (de)serialize
+json.dumps(
+    {
+        "timestamps": timestamps,
+    }
+)
